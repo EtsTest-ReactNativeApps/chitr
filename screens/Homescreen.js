c10a3b60941e91ec25f69771a9db42160ce6fc60
yamen <yamenadel3@gmail.com>
2020-03-03 10:41:15 +0000

    Add,search,get chits
    Adding Navigation bar-showing the recent chits-add an account (FIX)

--- screens/Homescreen.js
+++ screens/Homescreen.js
@@ -1,30 +1,39 @@
 import React, { Component } from 'react';
-import { Text, View,Button,StyleSheet,ActivityIndicator,FlatList } from 'react-native';
+import { Text, View,Button,TextInput,StyleSheet,ActivityIndicator,FlatList } from 'react-native';
 class HomeScreen extends Component{
     constructor(props){
         super(props);
         this.state = {
-        user : {
         given_name: '',
         family_name: '',
-        email: ''
-        },
+        text : '',
+        email: '',
         password: '',
         loginEmail:'',
         loginPass:'',
         chit_id: '',
         chit_content:'',
         timestamp: '',
-        location: {
         longitude: '',
         latitude: ''
-        }
-        };
+        };  
     }
+    
     Show =()=>{
-        this.props.navigation.navigate('StartPage');
+        this.props.navigation.navigate('UserProfile');
+      }
+    
+    handleGivenName = (text) => {
+        this.setState({ given_name: text })
     }
 
+    handleSearch = (text) => {
+        this.setState({ given_name: text })
+    }
+    
+//     handleSearch = (value) => {
+//     this.setState({ given_name: value}, this.search)
+//    }
     getChits(){
         return fetch('http://10.0.2.2:3333/api/v0.0.5/chits')
         .then((response) => response.json())
@@ -39,10 +48,24 @@ class HomeScreen extends Component{
         });
       }
 
+      search(){
+        return fetch('http://10.0.2.2:3333/api/v0.0.5/search_user?q='+this.state.given_name+' ')
+        .then((response) => response.json())
+        .then((responseJson) => {
+        this.setState({
+        isLoading: false,
+        userInfo: responseJson,
+        });
+        })
+        .catch((error) =>{
+        console.log(error);
+        });
+      }
+
       componentDidMount(){
         this.getChits();
        } 
-       
+          
  render(){
     if(this.state.isLoading){
         return(
@@ -51,10 +74,24 @@ class HomeScreen extends Component{
         </View>
         )
     }
-    
 return(
-
- <View style = {{ flex : 1, alignItems : 'flex-start', justifyContent : 'center'}}> 
+ <View style = {{ flex : 1, alignItems :'stretch'}}> 
+    <TextInput style = {styles.input} placeholder="Search for users" onChangeText={this.handleSearch} value={this.state.given_name}/>
+    <Button style = {styles.input} title="Search" 
+    onPress={() => this.search()}/>
+ 
+    <FlatList
+    data={this.state.userInfo}
+    renderItem={({item})=>
+  <View style={{flexDirection: 'row',flex:1,justifyContent:'space-evenly', alignItems : 'center' }}>
+    <Text style= {styles.chits}>{item.given_name}</Text>
+    <Text style= {styles.chits}>{item.family_name}</Text>
+    <Text style= {styles.chits}>{item.email}</Text>
+  </View>
+  }
+  keyExtractor={({id}, index) => id}
+ />
+ <View style = {{ flex : 10, justifyContent :'center'}}> 
     <Text style= {styles.title}>Most recent chits</Text>
     <FlatList
     data={this.state.chitsContent}
@@ -67,10 +104,12 @@ return(
   }
   keyExtractor={({id}, index) => id}
  />
+ </View>
  <View style = {{  alignItems : 'flex-start', justifyContent : 'flex-start'}}> 
- <Button  title="About Me"
+ <Button  title="About me"
  onPress={this.Show}/>
  </View>
+ 
  </View>
  );
  }
@@ -85,9 +124,15 @@ const styles = StyleSheet.create({
     },
     chits: {
     color: 'black',
-    fontSize: 20,
+    fontSize: 19,
     textShadowColor: 'gray',
     fontFamily: 'sans-serif',
     textShadowRadius: 19,
-    }
+    },
+    input: {
+        margin: 10,
+        height: 40,
+        borderBottomWidth: 1,
+        borderBottomColor: 'gray',
+      },
 });
